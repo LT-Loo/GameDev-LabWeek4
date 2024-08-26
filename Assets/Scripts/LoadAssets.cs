466c01a163aea2a01fb7e390cebc24da789b846a
LT Loo <lertheng@gmail.com>
2024-08-26 18:40:34 +1000

    70 percent complete

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
+    [SerializeField] private GameObject blueObj;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(redObj, new Vector3(2.0f, 0.0f, 0.0f), Quaternion.identity);
+        Instantiate(blueObj, new Vector3(-2.0f, 0.0f, 0.0f), Quaternion.identity);
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
